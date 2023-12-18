c47956e755632cc3afc0f2894194e9f6945b59ed
xdm5225 <nyan.dog20@gmail.com>
2023-12-19 00:11:36 +0100

    basic lotto gui

--- Dario/lotto/lotto_tk.py
+++ Dario/lotto/lotto_tk.py
@@ -1,44 +1,80 @@
-# from lotto_6aus49 import *
+import random
 import tkinter as tk
 
 
-def on_button_click(button_nr):
-    if button_nr not in selected_buttons:
+def update_label_text(label, text):
+    label.config(text=text)
+
+
+def button_lock_number(button_nr):
+    if button_nr not in selected_winners:
         # Check if there are already 6 selected buttons
-        if len(selected_buttons) >= 6:
+        if len(selected_winners) >= 6:
             # If there are 6 or more selected buttons, deselect the first one
-            first_selected = selected_buttons.pop(0)
+            first_selected = selected_winners.pop(0)
             buttons[first_selected - 1].config(bg="SystemButtonFace")  # Change the color back to the default
 
         # Update the list and color the clicked button
-        selected_buttons.append(button_nr)
+        selected_winners.append(button_nr)
         buttons[button_nr - 1].config(bg="green")
 
     # If the button is already selected, deselect it
     else:
-        selected_buttons.remove(button_nr)
+        selected_winners.remove(button_nr)
         buttons[button_nr - 1].config(bg="SystemButtonFace")
+
+    update_label_text(validate_label, "Choice is valid!" if validate(selected_winners) else "Choice is invalid!")
+
+
+def button_lock_in():
+    if validate(selected_winners):
+        amount = sum(1 for i in selected_winners if i in act_winners)
+        update_label_text(win_label, f"Du hast {amount} richtig.")
+
+
+def validate(lst):
+    return len(lst) == len(set(lst)) and len(set(lst)) == 6
+
+
+def get_winners():
+    regular_numbers = [random.randint(1, 49) for _ in range(6)]
+    superzahl = random.randint(1, 10)
+    return regular_numbers, superzahl
 
 
 root = tk.Tk()
 
-label = tk.Label(root, text="Hello, Tkinter!")
+label = tk.Label(root, text="Lotto 6 aus 49")
 label.grid(row=0, column=0, columnspan=7)
 
 buttons = []
-selected_buttons = []
+selected_winners = []
+selected_sz = []
 
+act_winners, sz_winners = get_winners()
+
+while not validate(act_winners):
+    act_winners, _ = get_winners()
 
 for i in range(7):
     for j in range(7):
         button_number = i * 7 + j + 1
-        button = tk.Button(root, text=button_number, command=lambda num=button_number: on_button_click(num))
+        button = tk.Button(root, text=button_number, command=lambda num=button_number: button_lock_number(num))
         button.grid(row=i + 2, column=j, sticky="nsew")  # Use sticky to make buttons expand
         buttons.append(button)
+
+lock_button = tk.Button(root, text="Lock in", command=button_lock_in)
+lock_button.grid(row=11, column=0, columnspan=7, sticky="nsew")
+
+validate_label = tk.Label(root, text="")
+validate_label.grid(row=10, column=0, columnspan=7)
+
+win_label = tk.Label(root, text="---Result---")
+win_label.grid(row=14, column=0, columnspan=7)
 
 # Set a common minsize for all columns and rows to ensure uniform size
 for i in range(7):
     root.grid_columnconfigure(i, minsize=50)
-    root.grid_rowconfigure(i + 2, minsize=50)
+    root.grid_rowconfigure(i + 10, minsize=50)
 
 root.mainloop()
