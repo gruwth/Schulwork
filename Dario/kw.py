ceefcf8ba5b83f6650bb50ada4566aeba5efbc71
xdm5225 <nyan.dog20@gmail.com>
2024-02-21 12:49:21 +0100

    Kalenderwochen rechner (oder so)

---
+++ Dario/kw.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import locale
+
+
+locale.setlocale(locale.LC_TIME, 'de_DE')
+
+now = datetime.now()
+
+formatted_time = now.strftime("%A, %d.%m.%Y %H:%M")
+calender_week = now.isocalendar()[1]
+
+print(formatted_time, "\nKW:", calender_week)
