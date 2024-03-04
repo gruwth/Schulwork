79485f53105b39cfd2dc9569c4e86b2b910b757d
Mika Ennen <mika.ennen@gmail.com>
2024-03-04 13:31:20 +0100

    Refactor input prompts and print statements to use f-strings

--- Mika/Datenstrukturen.py
+++ Mika/Datenstrukturen.py
@@ -1,13 +1,13 @@
 def namenseingabe():
     names = []
     for i in range(5):
-        name = input("Bitte gib den Namen der {}-ten Person ein: ".format(i+1))
+        name = input(f"Bitte gib den Namen der {i+1}-ten Person ein: ")
         names.append(name)
     return names
 
 def liste_ausgeben(names):
     reversed_names = names[::-1]
-    print("Die Liste der Namen in umgekehrter Reihenfolge lautet: {}".format(reversed_names))
+    print(f"Die Liste der Namen in umgekehrter Reihenfolge lautet: {reversed_names}")
 
 def neuen_namen_hinzufuegen(names):
     new_name = input("Bitte gib einen neuen Namen ein, der hinzugefügt werden soll: ")
@@ -16,9 +16,9 @@ def neuen_namen_hinzufuegen(names):
 def namen_ueberpruefen(names):
     name_to_check = input("Bitte gib einen Namen ein, um zu überprüfen, ob er in der Liste enthalten ist: ")
     if name_to_check in names:
-        print("{} ist in der Liste enthalten.".format(name_to_check))
+        print(f"{name_to_check} ist in der Liste enthalten.")
     else:
-        print("{} ist nicht in der Liste enthalten.".format(name_to_check))
+        print(f"{name_to_check} ist nicht in der Liste enthalten.")
 
 # Hauptprogramm
 names = namenseingabe()
