3c5f6c4a1bf8baca0519516ff0507644785723ff
xdm5225 <nyan.dog20@gmail.com>
2024-04-18 07:50:45 +0200

    Refactor mouse movement and focus switching logic in Dario/^-^.py

--- Dario/^-^.py
+++ Dario/^-^.py
@@ -1,40 +1,55 @@
-import pyautogui
-from pynput import keyboard
 import time
+import psutil
+import pygetwindow
+import traceback
 
+#4 minutes is selected because it looks like "Away" status is set at 5 minutes, but shows you ~2 minutes away.
+#4 minutes seem to work perfectly fine in avoiding that
+waitfor = 2 * 60
+jiggles = 0
+skips = 0
+notfound = 0
 
-stop_program = False
+# Function to switch focus to the specified application
+def switch_focus_to_app():
+    global jiggles, skips, notfound
+    try:
+        for process in psutil.process_iter(attrs=['pid', 'name']):
+            if process.name().lower() == 'teams.exe' or process.name().lower() == 'ms-teams.exe':
+                app_window = pygetwindow.getWindowsWithTitle('Excel | Microsoft Teams')
+                if app_window:
+                    #Jiggle only if window is not already active and maximized (most likely already in focus)
+                    if app_window[0].isActive and app_window[0].isMaximized:
+                        skips = skips + 1
+                        print(f"Jiggle skipped {skips} times")
+                    else:
+                        #If it's minimized - restore, which will take focus
+                        if app_window[0].isMinimized:
+                            app_window[0].restore()
+                        else:
+                            #If not minimized - activate
+                            app_window[0].activate()
+                        time.sleep(2)
+                        app_window[0].minimize()
+                        jiggles = jiggles + 1
+                        print(f"Jiggled Teams {jiggles} times")
+                    return True
+        notfound = notfound + 1
+        print(f"Teams was not found {notfound} times")
+    except Exception:
+        print("There was some error but we up")
+    return False
 
+waitforOrig = waitfor
+print(f"Teams Jiggler initiated")
+while True:
+    # Sleep for 3 minutes after launching the script
+    time.sleep(waitfor)
 
-def move_mouse(x, y, duration=0.25):
-    pyautogui.moveRel(x, y, duration=duration)
-
-
-def on_press(key):
-    global stop_program
-    if key == keyboard.Key.esc:
-        stop_program = True
-
-
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
-
-
-def main():
-    while not stop_program:
-        move_mouse(50, 0)
-        if stop_program:
-            break        
-        move_mouse(0, 50)
-        if stop_program:
-            break       
-        move_mouse(-50, 0)
-        if stop_program:
-            break        
-        move_mouse(0, -50)
-        if stop_program:
-            break
-
-if __name__ == "__main__":
-    main()
-
+    # Switch focus to the first application
+    if switch_focus_to_app():
+        waitfor = waitforOrig
+    else:
+        #If we fail, reduce wait time until we succeed
+        waitfor = 30
+print(f"Teams Jiggler loop ended")
